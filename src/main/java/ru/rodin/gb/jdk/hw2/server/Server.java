424aac3dcf551665a45e02e496ed4ccf7379d97d
Igor Rodin <igreen1974@gmail.com>
2023-11-26 01:02:33 +0300

    Init hw 2

---
+++ src/main/java/ru/rodin/gb/jdk/hw2/server/Server.java
@@ -0,0 +1,11 @@
+package ru.rodin.gb.jdk.hw2.server;
+
+public class Server {
+    public void start() {
+        System.out.println("Server started");
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
